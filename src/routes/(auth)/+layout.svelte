3e526a1837e8a7cfaf47f6fb72e2a2eeab997a26
Ricky Rivas <rickyrivas918@gmail.com>
2025-03-19 18:30:45 -0500

    auth layout styles

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+	import Logo from '$lib/Logo.svelte';
 	import type { LayoutProps } from './$types';
 	const { children }: LayoutProps = $props();
 </script>
 
 <main>
 	<section id="auth-hero">
+		<a id="logo" href="/"><Logo /></a>
 		<div class="auth-content">
 			{@render children()}
 		</div>
+		<p class="auth-content-footer">Rivas Web Designs, LLC &#169; {new Date().getFullYear()}</p>
 	</section>
 </main>
